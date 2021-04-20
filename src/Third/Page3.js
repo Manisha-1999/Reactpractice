b4ee639db825f88ec956fd29427127d29386c43b
Manisha <manishadey2020@gmail.com>
2021-04-20 21:30:20 +0530

    my first commit

---
+++ src/Third/Page3.js
@@ -0,0 +1,10 @@
+import {Component} from 'react';
+class Page3 extends Component {
+    componentDidMount (){
+        console.log("hello");
+    }
+    render(){
+        return <h1>Welcome to Page3 </h1>
+    }
+}
+export default Page3;
